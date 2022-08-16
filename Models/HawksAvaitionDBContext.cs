a8dbaf68b1ff723e808a327fab13d557251b3dbb
Nikhileswara Sri Venkat Muraharirao <99648180+NikhileshNikky@users.noreply.github.com>
2022-08-16 22:41:59 +0530

    Models

---
+++ Models/HawksAvaitionDBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AirlineReservationSystem.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace AirlineReservationSystem.Models
+{
+    public class HawksAvaitionDBContext : DbContext
+    {
+        public HawksAvaitionDBContext(DbContextOptions<HawksAvaitionDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+        public DbSet<Bookings> Bookings { get; set; }
+        public DbSet<Flights> Flights { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Airports> Airports { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<ExceptionLog> ExceptionLog { get; set; }
+
+        public virtual void sp_DeleteFlight()
+        {
+            //HawksAvaitionDBContext.ExecuteFunction("[dbo].[DeleteOldFlights]");
+        }
+    }
+}
